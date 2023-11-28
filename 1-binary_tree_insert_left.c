e4a087316036dabb6242ebed31b1736057dfbdff
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-11-28 11:14:36 +0200

    inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,12 +4,13 @@
  * binary_tree_insert_left - inserts a node as the lsft-child of another node
  * @parent: is a pointer to the node to insert the left-child in
  * @value: the value to store in the new node
- * Return: NULL on failure or NULL is parent is NULL or a pointer to the created node
+ * Return: NULL on failure or NULL is parent is NULL
+ *         a pointer to the created node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	
+
 	if (parent == NULL)
 		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
